26c5a57fa8f8918c1cf0a265c55c3512cf80cd45
Bernd Malle <bernd.malle@gmail.com>
2016-01-25 01:02:39 +0100

    added typings folder

---
+++ src/core/GenHierarchies.ts
@@ -0,0 +1,21 @@
+interface GenHierarchy {
+	
+}
+
+// We need no explicit one for ordinal (continuous) values
+
+// We need one for string values
+interface StringGenHierarchy {
+	
+}
+
+
+
+
+
+
+
+
+
+
+export { StringGenHierarchy };
---
+++ test/core/GenHierarchyTests.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import * as chai from 'chai';
+import * as $GH from '../../src/core/GenHierarchies';
+
+var expect = chai.expect;
+var StrGH : $GH.StringGenHierarchy;
+
+
+describe('Generalization Hierarchies Tests: ', () => {
+	
+	describe('Basic instantiation tests', () => {
+		
+	
+		
+	});
+	
+});
